ba83922eec6edab375611ff95e75cafa2fd46c2b
businessProjectVision <141189660+businessProjectVision@users.noreply.github.com>
2025-01-01 17:16:32 -0500

    Update gotopost.js 1-20

--- js/gotopost.js
+++ js/gotopost.js
@@ -5,3 +5,83 @@ document.getElementById('scroll-to-footer').addEventListener('click', function()
 document.getElementById('alarm-ativo').addEventListener('click', function() {
             document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
  });
+
+document.getElementById('imgalarm1').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm2').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm3').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm4').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm5').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm6').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm7').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm8').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm9').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm10').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm11').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm12').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm13').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm14').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm15').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm16').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm17').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm18').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm19').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
+
+document.getElementById('imgalarm20').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
