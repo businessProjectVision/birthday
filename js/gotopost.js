e5e76a4fed4e4fec75025d64729519517af52150
businessProjectVision <141189660+businessProjectVision@users.noreply.github.com>
2024-12-31 22:46:04 -0500

    Update gotopost.js

--- js/gotopost.js
+++ js/gotopost.js
@@ -1,3 +1,7 @@
 document.getElementById('scroll-to-footer').addEventListener('click', function() {
             document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
  });
+
+document.getElementById('alarm-ativo').addEventListener('click', function() {
+            document.getElementById('post').scrollIntoView({ behavior: 'smooth' });
+ });
